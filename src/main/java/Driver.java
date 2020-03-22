f0e12a0327cea74eaefb3d58d552ecd4fcf79e55
igor <ciken_@mail.ru>
2020-03-22 14:53:59 +0200

    driver option

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,7 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Driver {
-    public static void main(String[] args) {
+
+    private static WebDriver driver;
+
+    public static void startDriver(){
+        System.setProperty("webdriver.chrome.driver",
+                "D:\\chromedriver.exe");
 
 
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+    }
+
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
+
+    public static void quit(){
+        driver.quit();
     }
 
 }
